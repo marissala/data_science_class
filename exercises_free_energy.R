13d8b3e9e5b6d951a41458b8d196a56fd68cdcea
Maris Sala <marissala.o7@gmail.com>
2020-03-05 23:30:56 +0100

    Code for exercise 1-3 (not 4 and 5)

---
+++ exercises_free_energy.R
@@ -0,0 +1,102 @@
+pacman::p_load(dplyr, ggplot2)
+
+#### EXERCISE 1
+
+# g: non-linear function relating the average light intenstiy with the size
+g <- function(v) {
+  return(v^2)
+} 
+# size/diameter of food item
+v = seq(0.01, 5, by = 0.01)
+# u: estimate of light intensity7
+u = 2
+var_u = 1
+
+## prior
+v_p = 3
+var_p = 1
+
+# p(v)
+p_v <- dnorm(v, v_p, var_p^2)
+
+data.frame(x = v, y = p_v) %>% 
+  ggplot(aes(x, y)) +
+  geom_line() +
+  theme_bw() +
+  labs(x = 'v', y = 'p(v)')
+
+# p(u|v)
+p_u_v <- dnorm(u, g(v), var_u^2)
+
+numerator <- (p_v * p_u_v)
+denominator <- sum(numerator * 0.01)
+
+p_v_u <- numerator / denominator
+
+plot <- data.frame(x = v, y = p_v_u) %>% 
+  ggplot(aes(x, y)) +
+  geom_line() +
+  theme_bw() +
+  labs(x = 'v', y = 'p(v|u)')
+
+plot
+
+
+#### EXERCISE 2
+f_der <- function(phi) {
+  epsilon_p <- (v_p - phi) / var_p
+  epsilon_u <- (u-g(phi)) / var_u ## what is the g(phi)?
+  #g: non-linear function relating the average light intenstiy with the size
+  g_der <- 2*phi
+  
+  f_d <- epsilon_p + epsilon_u * g_der
+  return(f_d)
+}
+
+time_steps = 500
+
+# most likely size of food
+phi <- array(0, c(time_steps))
+phi[1] <- v_p
+
+delta_t <- 0.01
+
+for (i in 2:time_steps) {
+  phi[i] <- phi[i-1] + delta_t * f_der(phi[i-1])
+}
+
+data.frame(x = (seq(1, time_steps, by = 1)/100), y = phi) %>% 
+  ggplot(aes(x = x, y= y)) + 
+  geom_point() +
+  geom_line() +
+  labs(x = "Time", y = "phi")
+
+plot(phi)
+
+#### EXERCISE 3
+
+#simulate the model
+phi <- array(0, c(time_steps))
+epsilon_p <- array(0,c(time_steps))
+epsilon_u <- array(0,c(time_steps))
+phi[1] = v_p
+epsilon_p[1] = 0
+epsilon_u[1] = 0
+
+for (i in 2:time_steps) {
+  phi[i] <- phi[i-1] + delta_t * (-epsilon_p[i-1] + epsilon_u[i-1] * (2*phi[i-1]))
+  epsilon_p[i] <- epsilon_p[i-1] + delta_t * (phi[i-1] - v_p - var_p*epsilon_p[i-1])
+  epsilon_u[i] <- epsilon_u[i-1] + delta_t * (u - g(phi[i-1]) - var_u*epsilon_u[i-1])
+}
+
+plot(phi)
+plot(epsilon_p)
+plot(epsilon_u)
+plot(phi,epsilon_p)
+
+#### EXERCISE 4
+
+
+
+
+#### EXERCISE 5
